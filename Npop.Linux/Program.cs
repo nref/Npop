1465dfb66db13ed8eac82c5e9dfb16bc2caea3eb
Doug Slater <doug@slater.dev>
2024-09-20 21:45:51 -0400

    Add Linux

---
+++ Npop.Linux/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using Tmds.DBus;
+
+// get app icon "sample.png" in exe dir
+var dir = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location ?? "");
+Console.WriteLine(dir);
+
+var connection = new Connection(Address.Session);
+await connection.ConnectAsync();
+
+var notifier = connection.CreateProxy<INotification>("org.freedesktop.Notifications", "/org/freedesktop/Notifications");
+
+await notifier.NotifyAsync("appName", 0, $"{dir}/sample.png", "summary", "body", [], new Dictionary<string, object>() {}, 5000);
+
+await Task.Delay(TimeSpan.FromSeconds(10));
+connection.Dispose();
+
+[DBusInterface("org.freedesktop.Notifications")]
+public interface INotification : IDBusObject
+{
+   Task<uint> NotifyAsync(string appName, uint replacesId, string appIcon, string summary, string body,
+      string[] actions, IDictionary<string, object> hints, int expireTimeout);
+}
